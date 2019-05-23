c1a6f7d4b839ee86029fd76d235f26525d517fa8
Lucas Campelo <luc4pc@gmail.com>
2019-05-23 01:55:59 -0300

    Adicionando comentários nos arquivos

--- carrinho.js
+++ carrinho.js
@@ -1,26 +1,10 @@
-var carrinho=0;
-var quant=0;
-
-function btnProduto(x){
-    carrinho += parseFloat(x)
-    var resultado = carrinho.toFixed(2);
-    quant++;
-    atualizarCarrinho();
-}
-function atualizarCarrinho(){
-    var pcarrinho = document.getElementsByClassName("qtdCar")
-    for (let i = 0; i < pcarrinho.length; i++) {pcarrinho[i].innerHTML = quant}
-   
-    // pcarrinho[1].innerHTML = quant
-    // pcarrinho[1].innerHTML = quant
-}
-
+    // Dando função aos botões.
 var btnsCarrinho = document.getElementsByClassName('botao')
 for (var i = 0; i < btnsCarrinho.length; i++) {
     var button = btnsCarrinho[i]
     button.addEventListener('click', addCarClicado)
 }
-
+// Pegando detalhes do produto e rodando algumas funções que estão relacionadas.
 function addCarClicado(event) {
     var button = event.target
     var prodt = button.parentElement.parentElement
@@ -30,21 +14,23 @@ function addCarClicado(event) {
     btnProduto(price)
     addCar(title,price,imageSrc)
     attCarTotal()
+    
 }
+//Adicionando os produtos ao carrinho
 function addCar(title, price, imageSrc) {
     var cartRow = document.createElement('tr')
     cartRow.classList.add('tableItens')
     var carPdt = document.getElementsByClassName('tablebody')[0]
-
     var carPdtNome = carPdt.getElementsByClassName('titulo-car')
     for (var i = 0; i < carPdtNome.length; i++) {
-        if (carPdtNome[i].innerText == title) {
+        // Mandando um alerta caso o produto escolhido esteja no carrinho.
+        if (carPdtNome[i].innerText == title) { 
             alert('Este produto já está no carrinho')
             return
         }
     }
-
-var cartRowctd = `
+                                    // Colocando os produtos a serem exibidos na tela, e mandando pro html.
+var cartRowctd = `                
                             <th scope="row"></th>
                             <td> <img src="${imageSrc}"width="99" height="99"> <span class="titulo-car">${title}</span></td>
                             <td class="itemPreco">${price}</td>
@@ -52,17 +38,19 @@ var cartRowctd = `
                             <button class="btn btn-danger btn-retirar" type="button">Retirar</button></td>
                         `
 
+        //Pegando os botões de retirar e mudar a quantidade de produtods.
         cartRow.innerHTML = cartRowctd
         carPdt.append(cartRow)
-
         cartRow.getElementsByClassName('btn-retirar')[0].addEventListener('click', retirarCartItem)
         cartRow.getElementsByClassName('cart-quantidade-input')[0].addEventListener('change', quantidadeMudou)
 }
+// Dando a funcionalidade de retirar produtos
 function retirarCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
     attCarTotal()
 }
+//Adicionar mais quantidades do mesmo produto.
 function quantidadeMudou(event) {
     var input = event.target
     if (isNaN(input.value) || input.value <= 0) {
@@ -70,7 +58,7 @@ function quantidadeMudou(event) {
     }
     attCarTotal()
 }
-
+// Pegando e calculando o valor do produto que esta dentro do carrinho.
 function attCarTotal() {
     var carpdtContainer = document.getElementsByClassName('tablebody')[0]
     var cartRows = carpdtContainer.getElementsByClassName('tableItens')
@@ -84,9 +72,9 @@ function attCarTotal() {
         var quantidade = elementoQtd.value
         total = total + (price * quantidade)
     }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('precoTotal')[0].innerText = '$' + total
+    document.getElementsByClassName('precoTotal')[0].innerText = '$' + total // Pega a string $ e coloca na tela
 }
+// Faz com que depois que o usuário faça a compra apareça uma string. Retira todos os produtos após a compra.
 document.getElementsByClassName('compra')[0].addEventListener('click', compraClicked)
 function compraClicked() {
     alert('Obrigado pela preferencia :)')
